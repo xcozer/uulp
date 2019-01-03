8de1aa965414c403262222d5dfcd08850bef5223
xcozer <6082790+xcozer@users.noreply.github.com>
2019-01-03 23:09:10 +0800

    fileinfo
    convert file mode to letters

--- ch3/fileinfo.c
+++ ch3/fileinfo.c
@@ -2,10 +2,13 @@
 // Created by xuechi on 2019-01-02.
 //
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
 void show_stat_info(char *, struct stat *);
+
+void mode_to_letters(int, char []);
 
 int main(int argc, char *argv[]) {
     struct stat info;
@@ -22,12 +25,35 @@ int main(int argc, char *argv[]) {
     return 1;
 }
 
-void show_stat_info(char *filename, struct stat *buf) {
-    printf("    mode: %o\n", buf->st_mode);
-    printf("   links: %d\n", buf->st_nlink);
-    printf("    user: %d\n", buf->st_uid);
-    printf("   group: %d\n", buf->st_gid);
-    printf("    size: %lld\n", buf->st_size);
-    printf(" modtime: %ld\n", buf->st_mtimespec.tv_sec);
+void show_stat_info(char *filename, struct stat *stat_ptr) {
+//    printf("    mode: %o\n", buf->st_mode);
+    char buf[10] = {};
+    mode_to_letters(stat_ptr->st_mode, buf);
+    printf("    mode: %s\n", buf);
+    printf("   links: %d\n", stat_ptr->st_nlink);
+    printf("    user: %d\n", stat_ptr->st_uid);
+    printf("   group: %d\n", stat_ptr->st_gid);
+    printf("    size: %lld\n", stat_ptr->st_size);
+    printf(" modtime: %ld\n", stat_ptr->st_mtimespec.tv_sec);
     printf("    name: %s\n", filename);
+}
+
+void mode_to_letters(int mode, char str[]) {
+    strcpy(str, "----------");
+
+    if (S_ISDIR(mode)) str[0] = 'd';
+    if (S_ISCHR(mode)) str[0] = 'c';
+    if (S_ISBLK(mode)) str[0] = 'b';
+
+    if (mode & S_IRUSR) str[1] = 'r';
+    if (mode & S_IWUSR) str[2] = 'w';
+    if (mode & S_IXUSR) str[3] = 'x';
+
+    if (mode & S_IRGRP) str[4] = 'r';
+    if (mode & S_IWGRP) str[5] = 'w';
+    if (mode & S_IXGRP) str[6] = 'x';
+
+    if (mode & S_IROTH) str[7] = 'r';
+    if (mode & S_IWOTH) str[8] = 'w';
+    if (mode & S_IXOTH) str[9] = 'x';
 }
