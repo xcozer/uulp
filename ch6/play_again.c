952d049115bc6d3e835faa276f051d1e394001a3
xcozer <6082790+xcozer@users.noreply.github.com>
2019-01-06 03:29:07 +0800

    play_again
    version 4

--- ch6/play_again.c
+++ ch6/play_again.c
@@ -2,51 +2,70 @@
 // Created by xuechi on 2019-01-06.
 //
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 #include <termios.h>
 
-#define QUESTION "Do you want another transaction"
+#define ASK "Do you want another transaction"
+#define TRIES 3
+#define SLEEPTIME 2
+#define BEEP putchar('\a')
 
-int get_response(char *);
+int get_response(char *, int);
+
+int get_ok_char();
 
 void tty_mode(int);
 
 void set_cr_noecho_mode();
+
+void set_nodelay_mode();
 
 int main() {
     int response;
 
     tty_mode(0);
     set_cr_noecho_mode();
+    set_nodelay_mode();
 
-    response = get_response(QUESTION);
+    response = get_response(ASK, TRIES);
 
     tty_mode(1);
 
     return response;
 }
 
-int get_response(char *question) {
+int get_response(char *question, int max_tries) {
     int input;
     printf("%s (y/n)?", question);
+    fflush(stdout);
     while (1) {
-        switch (input = getchar()) {
-            case 'y':
-            case 'Y':
-                return 0;
-            case 'n':
-            case 'N':
-            case EOF:
-                return 1;
+        sleep(SLEEPTIME);
+        input = tolower(get_ok_char());
+        if (input == 'y') {
+            return 0;
         }
+        if (input == 'n') {
+            return 1;
+        }
+        if (max_tries-- == 0) {
+            return 2;
+        }
+        BEEP;
     }
 }
 
 void tty_mode(int how) {
     static struct termios original_mode;
+    static int original_flags;
     if (how == 0) {
         tcgetattr(0, &original_mode);
+        original_flags = fcntl(0, F_GETFL);
     } else {
         tcsetattr(0, TCSANOW, &original_mode);
+        fcntl(0, F_SETFL, original_flags);
     }
 }
 
@@ -57,4 +76,19 @@ void set_cr_noecho_mode() {
     info.c_lflag &= ~ECHO;
     info.c_cc[VMIN] = 1;
     tcsetattr(0, TCSANOW, &info);
+}
+
+int get_ok_char() {
+    int c;
+    while ((c = getchar()) != EOF && strchr("yYnN", c) == NULL)
+        ;
+
+    return c;
+}
+
+void set_nodelay_mode() {
+    int term_flags;
+    term_flags = fcntl(0, F_GETFL);
+    term_flags |= O_NDELAY;
+    fcntl(0, F_SETFL, term_flags);
 }
