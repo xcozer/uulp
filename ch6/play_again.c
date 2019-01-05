c49339d745ea4281857d2b4546397c0748962ad1
xcozer <6082790+xcozer@users.noreply.github.com>
2019-01-06 02:25:54 +0800

    play_again
    version 2

--- ch6/play_again.c
+++ ch6/play_again.c
@@ -8,16 +8,28 @@
 
 int get_response(char *);
 
+void tty_mode(int);
+
+void set_crmode();
+
 int main() {
     int response;
+
+    tty_mode(0);
+    set_crmode();
+
     response = get_response(QUESTION);
+
+    tty_mode(1);
+
     return response;
 }
 
 int get_response(char *question) {
+    int input;
     printf("%s (y/n)?", question);
     while (1) {
-        switch (getchar()) {
+        switch (input = getchar()) {
             case 'y':
             case 'Y':
                 return 0;
@@ -25,6 +37,26 @@ int get_response(char *question) {
             case 'N':
             case EOF:
                 return 1;
+            default:
+                printf("\ncannot understand %c, ", input);
+                printf("Please type y or n \n");
         }
     }
+}
+
+void tty_mode(int how) {
+    static struct termios original_mode;
+    if (how == 0) {
+        tcgetattr(0, &original_mode);
+    } else {
+        tcsetattr(0, TCSANOW, &original_mode);
+    }
+}
+
+void set_crmode() {
+    struct termios info;
+    tcgetattr(0, &info);
+    info.c_lflag &= ~ICANON;
+    info.c_cc[VMIN] = 1;
+    tcsetattr(0, TCSANOW, &info);
 }
