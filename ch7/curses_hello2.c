c7e093c620e0cab279a0954917b983dc7c81dd4f
xcozer <6082790+xcozer@users.noreply.github.com>
2019-01-06 04:55:41 +0800

    curses demo 2

---
+++ ch7/curses_hello2.c
@@ -0,0 +1,25 @@
+//
+// Created by xuechi on 2019-01-06.
+//
+#include <stdio.h>
+#include <curses.h>
+
+int main() {
+    int i;
+
+    initscr();
+    clear();
+    for (i = 0; i < LINES; ++i) {
+        move(i, i + i);
+        if (i % 2 == 1) {
+            standout();
+        }
+        addstr("Hello, World");
+        if (i % 2 == 1) {
+            standend();
+        }
+        refresh();
+        getch();
+        endwin();
+    }
+}
